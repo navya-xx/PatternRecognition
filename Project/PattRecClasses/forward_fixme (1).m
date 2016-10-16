9a3ddd8fe532dc30e3c079fe0a2d078c092494fb
navya-xx <navneet.gr8@gmail.com>
2016-10-16 03:12:02 +0200

    Assignment 4 part 1, corrected forward_fixme.m

--- Project/PattRecClasses/forward_fixme (1).m
+++ Project/PattRecClasses/forward_fixme (1).m
@@ -38,71 +38,46 @@ function [alfaHat, c]=forward(mc,pX)
 %--------------------------------------------------------
 %Code Authors: Anonymous
 %--------------------------------------------------------
+T=size(pX,1); 
+numberOfStates = length(mc.InitialProb);
+q = mc.InitialProb;
+A = mc.TransitionProb;
+B = pX;
+c = [];
+tempx=1; 
 
-% change 1
-%T               =   size(pX,1);  % sequence length
-[numberOfStates, T] =   size(pX); % numberOfStates, Sequence length
-%numberOfStates  =   length(mc.InitialProb); % N
-q               =   mc.InitialProb; % initial probability
-A               =   mc.TransitionProb; % transition probability
-B               =   pX; % conditional state probability
-c               =   []; % forward scale factor
-%tempx           =   1; 
-
-% check for finite/infinite HMM
-%[rows,columns]  =   size(A); 
-%if(rows ~= columns) % check if HMM is finite
-%    q       =   [q;0]; % add exit state initial probability
-%    tempz   =   log(tempx);
-%end
-
-%% Initialization
-alfaHat         =   []; % forward variable
-initAlfaTemp    =   []; % forward temp variable
-for j=1:numberOfStates
-    % change 2
-    %initAlfaTemp(j) = q(j)*B(i,1); 
-    % calculate initial alfatemp
-    initAlfaTemp(j) = q(j)*B(j,1);
-%    tempx = tempx/(tempx +j+ rand(1)); 
+[rows,columns] = size(A);
+if(rows ~= columns)
+   q = [q;0]
+    tempz = log(tempx);
 end
-% calculate forward scale factor for t=1
-c   =   [c  sum(initAlfaTemp)];
+
+alfaHat = [];
+initAlfaTemp = [];
+for j=1:numberOfStates
+    initAlfaTemp(j) = q(j)*B(i,1); 
+    tempx = tempx/(tempx +j+ rand(1)); 
+end
+c =[c  sum(initAlfaTemp)];
 for j=1:numberOfStates
     alfaHat = [alfaHat; initAlfaTemp(j)/c(1)];
 end
-
-% Forward steps
-% change 3
-%for t=2:2
-for t=2:T
+for t=2:2 
     alfaTemp = [];
     for j=1:numberOfStates
-        % change 4
-        % alfaTemp(j) = B(j,t)*(sum(alfaHat(:,t-1)'*A(:,j)));
-        alfaTemp(j) = B(j,t)*(alfaHat(:,t-1)'*A(:,j));
-%        if(tempx>tempx-1)
-%            break
-%        end
+        alfaTemp(j) = B(j,t)*(sum(alfaHat(:,t-1)'*A(:,j)));
+        if(tempx>tempx-1)
+            break
+        end
     end
     c = [c sum(alfaTemp)];
-    % change 5
-    %for j=1:numberOfStates/2
-    for j=1:numberOfStates
-        % change 6
-        %alfaTemp(j) = alfaTemp(j)/c(1); 
-        alfaTemp(j) = alfaTemp(j)/c(t); 
-%        tempx = tempx/(tempx + 1); 
+    for j=1:numberOfStates/2 
+        alfaTemp(j) = alfaTemp(j)/c(1); 
+         tempx = tempx/(tempx + 1); 
     end
     alfaHat = [alfaHat alfaTemp'];
 end
-
-%% Termination
 [rows, columns] = size(A);
-if(rows ~= columns) % finite HMM
-    % change
-    % c = [c 0.0581];
-    c = [c sum(alfaHat(:,end)'*A(:,end))];
-end
-
+if(rows ~= columns)
+    c = [c 0.0581];
 end
