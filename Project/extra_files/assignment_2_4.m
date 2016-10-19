523c662a2a3d4090f797d0104676491a0fdf96c1
LarsSebastian <lars.kuger@rwth-aachen.de>
2016-10-19 13:57:43 +0200

    Move assignment_2_x.m files to extra files folder

---
+++ Project/extra_files/assignment_2_4.m
@@ -0,0 +1,45 @@
+%% Assignment 2.4
+% A copy of your MatLab code that plots and compares correlation ma-
+% trices for the spectral and cepstral coefficient series.
+
+addpath(strcat(pwd,'/Sounds'));
+addpath(strcat(pwd,'/Songs'));
+addpath(strcat(pwd,'/GetSpeechFeatures'));
+% extract audio from female.wav
+[Yf,Ffs] = audioread('female.wav');
+
+% spectogram params
+X = 30;
+winlength = X * 1e-3; % corresopnd to X ms
+ncel = 13;
+
+% mfcc, spectogram calculation
+[mfcc_fem, spectogram_fem ,f ,t ] =   GetSpeechFeatures(Yf,Ffs,winlength,ncel);
+
+% calculate correlation of spectrum and ceptrum
+spec_corr = corr(10*log10(spectogram_fem)');
+
+% Normalize cepstrum values
+mfcc_fem_norm = zeros(size(mfcc_fem));
+for i = 1:size(mfcc_fem,2)
+    m = mean(mfcc_fem(:,i));
+    v = var(mfcc_fem(:,i));
+    mfcc_fem_norm(:,i) = (mfcc_fem(:,i)-m)./sqrt(v);
+end
+
+cepc_corr = corr(mfcc_fem_norm');
+
+% comparison of covariance matrices
+figure;
+imagesc(t,f,spec_corr)
+colormap gray
+title('Correlation matrix of spectrogram')
+xlabel('Frequency [Hz]');
+ylabel('Frequency [Hz]');
+
+figure;
+imagesc(cepc_corr)
+colormap gray
+title('Correlation matrix of cepstrogram')
+xlabel('Coefficient #');
+ylabel('Coefficient #');
